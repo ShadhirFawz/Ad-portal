9d5772858c123b40085fdb5ed812f7c43139c81f
Shadhir Fawz <it22237118@my.sliit.lk>
2026-08-10 10:22:36 +0530

    fix: schema name format mismatch and introduce object wrapper

--- backend/src/main/java/com/marketplace/marketplace/common/security/config/SecurityConfiguration.java
+++ backend/src/main/java/com/marketplace/marketplace/common/security/config/SecurityConfiguration.java
@@ -1,9 +1,16 @@
 package com.marketplace.marketplace.common.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationPropertiesScan(basePackageClasses = SecurityConfiguration.class)
 public class SecurityConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
